63c01be4f61df934bdc83f6ecea68221df222e3f
Dev <devdkumar666@gmail.com>
2023-06-25 03:22:54 +0530

    Student class added

--- src/com/first/program/Firstprogram.java
+++ src/com/first/program/Firstprogram.java
@@ -1,6 +1,6 @@
 package com.first.program;
 
-public class FIrstProgram {
+public class Firstprogram {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
---
+++ src/com/first/program/Student.java
@@ -0,0 +1,27 @@
+package com.first.program;
+
+public class Student {
+int roll_no;
+String first_name;
+String last_name;
+public int getRoll_no() {
+	return roll_no;
+}
+public void setRoll_no(int roll_no) {
+	this.roll_no = roll_no;
+}
+public String getFirst_name() {
+	return first_name;
+}
+public void setFirst_name(String first_name) {
+	this.first_name = first_name;
+}
+public String getLast_name() {
+	return last_name;
+}
+public void setLast_name(String last_name) {
+	this.last_name = last_name;
+}
+
+
+}
